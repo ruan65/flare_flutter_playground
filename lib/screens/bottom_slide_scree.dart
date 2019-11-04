9e3272c19313df2643e0c48cd359f90b8bbb71c6
ruan65 <ruan65@gmail.com>
2019-11-04 20:26:54 +0300

    pop is fine

--- lib/screens/bottom_slide_scree.dart
+++ lib/screens/bottom_slide_scree.dart
@@ -1,19 +1,43 @@
 import 'package:flutter/material.dart';
 
-class BottomSlideScreen extends StatelessWidget {
+class BottomSlideScreen extends StatefulWidget {
+  @override
+  _BottomSlideScreenState createState() => _BottomSlideScreenState();
+}
+
+class _BottomSlideScreenState extends State<BottomSlideScreen> {
+  ScrollController _controller = ScrollController();
+  bool poped = false;
+
   @override
   Widget build(BuildContext context) {
+    _controller.addListener(() {
+      if (_controller.offset < -50 && !poped) {
+        poped = true;
+        Navigator.of(context).pop();
+      }
+    });
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.orange,
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.close), onPressed: () {
-            Navigator.of(context).pop();
-          },)
+          IconButton(
+            icon: Icon(Icons.close),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          )
         ],
       ),
-      body: Container(
-        color: Colors.amberAccent,
+      body: SingleChildScrollView(
+        controller: _controller,
+        child: Column(
+          children: List<Widget>.generate(
+              30,
+              (i) => ListTile(
+                    title: Text('item $i'),
+                  )),
+        ),
       ),
     );
   }
